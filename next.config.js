8863b2963a133a82468145d136a273304a48b6cd
bjrn <bjorn.rixman@docly.com>
2020-10-12 22:59:26 +0200

    add tinacms config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   env: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     REPO_FULL_NAME: process.env.REPO_FULL_NAME,
-    BASE_BRANCH: process.env.BASE_BRANCH,
+    BASE_BRANCH: 'main', // process.env.BASE_BRANCH,
   },
   // ...
 };
